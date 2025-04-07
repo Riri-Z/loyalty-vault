635a84638f8e4a58bad01a0ca7e105fb1892a939
Riri-Z <riri.zamor@gmail.com>
2025-04-07 17:58:00 +0200

    Feat : Add loader

---
+++ components/ui/Loading.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import LottieView from "lottie-react-native";
+import { SafeAreaView, StatusBar } from "react-native";
+import { useColor } from "@/providers/ThemeProvider";
+
+const loader = require("../../assets/lottie/loader.json");
+
+export default function Loading() {
+	const { bgColor } = useColor();
+
+	return (
+		<>
+			<StatusBar translucent backgroundColor="transparent" />
+			<SafeAreaView style={{ flex: 1, backgroundColor: bgColor }}>
+				<LottieView
+					source={loader}
+					style={{ width: "50%", height: "100%", alignSelf: "center", backgroundColor: bgColor }}
+					autoPlay
+					loop
+				/>
+			</SafeAreaView>
+		</>
+	);
+}
--- components/ui/ViewContainer.tsx
+++ components/ui/ViewContainer.tsx
@@ -1,15 +1,20 @@
 import { PropsWithChildren } from "react";
-import { StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 import { useColor } from "@/providers/ThemeProvider";
 
 export default function ViewContainer({ children }: PropsWithChildren) {
 	const { bgColor } = useColor();
 
-	return <View style={[styles.conainer, { backgroundColor: bgColor }]}>{children}</View>;
+	return (
+		<>
+			<StatusBar translucent backgroundColor="transparent" />
+			<View style={[styles.container, { backgroundColor: bgColor }]}>{children}</View>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-	conainer: {
+	container: {
 		flex: 1,
 		padding: 20,
 		gap: 10,
